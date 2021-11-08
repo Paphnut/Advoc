7f59bd52ef57a3b0ca7bdcb4b98f488e0b57b270
paphnut <blagar.pavel@ya.ru>
2021-11-09 01:57:28 +0500

    Добавил вкладки

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from kivymd.app import MDApp
 from kivymd.theming import ThemableBehavior
 from kivymd.uix.list import OneLineIconListItem, MDList
 
+from kivy.uix.scrollview import ScrollView
+from kivymd.uix.tab import MDTabsBase
+from kivymd.icon_definitions import md_icons
+from kivymd.font_definitions import fonts
+
 KV = '''
 # Menu item in the DrawerList list.
 <ItemDrawer>:
@@ -73,6 +78,9 @@ Screen:
                         elevation: 10
                         left_action_items: [['menu', lambda x: nav_drawer.set_state('open')]]
 
+                    MDTabs:
+                        id: tabs
+                        
                     Widget:
 
         MDNavigationDrawer:
@@ -82,6 +90,8 @@ Screen:
                 id: content_drawer
 '''
 
+class Tab(ScrollView, MDTabsBase):
+    pass
 
 class ContentNavigationDrawer(BoxLayout):
     pass
@@ -120,6 +130,12 @@ class AdvocApp(MDApp):
         for icon_name in icons_item.keys():
             self.root.ids.content_drawer.ids.md_list.add_widget(
                 ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+            )
+#        for name_tab in list(md_icons.keys())[15:30]:
+#            self.root.ids.tabs.add_widget(Tab(icon=name_tab, title=name_tab))
+        for icon_name, name_tab in  icons_item.items():
+            self.root.ids.tabs.add_widget(
+                Tab(text=f" [ref={name_tab}][font={fonts[-1]['fn_regular']}]{md_icons[icon_name]}[/font][/ref] {name_tab}")
             )
 
 
